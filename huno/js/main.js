0137a9084563a60403008aae15a150b2ea7131a4
letian <sunlt1699@gmail.com>
2015-11-29 20:42:48 +0800

    modify huno demo

--- huno/js/main.js
+++ huno/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
-  if (window.location.pathname != "/") {
+  if (window.location.pathname != "/huno/") {       // if hexo in subdir of site, should change this line
     $('.panel-cover').addClass('panel-cover--collapsed');
   }
 
@@ -26,3 +26,4 @@ $(document).ready(function() {
   });
 
 });
+
